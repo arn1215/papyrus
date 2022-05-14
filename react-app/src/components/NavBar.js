9e834b63b53d4133086aa1964ac13cbc6f9a47aa
Ali <ali.naqvi.dev@gmail.com>
2022-05-13 20:17:30 -0500

    styling nav

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -6,31 +6,19 @@ import LogoutButton from './auth/LogoutButton';
 const NavBar = () => {
   return (
     <nav>
-      <ul>
-        <li>
           <NavLink to='/' exact={true} activeClassName='active'>
             Home
-          </NavLink>
-        </li>
-        <li>
+          </NavLink>  
           <NavLink to='/login' exact={true} activeClassName='active'>
             Login
           </NavLink>
-        </li>
-        <li>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
-        </li>
-        <li>
           <LogoutButton />
-        </li>
-      </ul>
     </nav>
   );
 }
