147b07aede1a6daf927947a98a894d8f892e2b36
Ali <ali.naqvi.dev@gmail.com>
2022-05-15 21:27:42 -0500

    got one notebook with one note seeded for marnie

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .notes import seed_notes, undo_notes
+from .notebooks import seed_notebooks, undo_notebooks
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -10,11 +12,16 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_notebooks()
+    seed_notes()
+
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_notes()
+    undo_notebooks()
     undo_users()
     # Add other undo functions here
---
+++ app/seeds/notebooks.py
@@ -0,0 +1,21 @@
+from app.models import db, NoteBook
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_notebooks():
+    demoNotebook = NoteBook(
+        id='2', title='marnies notebook', userId='2')
+
+
+    db.session.add(demoNotebook)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE the users table.
+# SQLAlchemy doesn't have a built in function to do this
+# TRUNCATE Removes all the data from the table, and RESET IDENTITY
+# resets the auto incrementing primary key, CASCADE deletes any
+# dependent entities
+def undo_notebooks():
+    db.session.execute('TRUNCATE notebooks RESTART IDENTITY CASCADE;')
+    db.session.commit()
