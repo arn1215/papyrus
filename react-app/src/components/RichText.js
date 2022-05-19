fff0fbb41aee3ab1d5f4210e61b78fc8e4b21157
Ali <ali.naqvi.dev@gmail.com>
2022-05-19 00:39:51 -0500

    added theme

--- react-app/src/components/RichText.js
+++ react-app/src/components/RichText.js
@@ -15,6 +15,8 @@ const RichText = () => {
   const params = useParams()
   const note = useSelector(state => state.notes[params.id])
   const [content, setContent] = useState(note?.content)
+  const [color, setColor] = useState('#ebebeb')
+  const [fontColor, setFontColor] = useState('darkslate')
 
   const onClick = async () => { 
     
@@ -27,13 +29,23 @@ const RichText = () => {
     await dispatch(editNote(updatedNote))
   }
 
+  const onTheme = () => {
+    if (color === 'rgb(71, 64, 61)') {
+      setColor('#ebebeb')
+      setFontColor('black')
+    } else {
+      setFontColor('aliceblue')
+      setColor('rgb(71, 64, 61)')
+    }
+  }
+
   const handleContent = e => {
     console.log(e)
     setContent(e)
   }
 
   return (
-    <div id='container' >
+    <div id='container' style={{backgroundColor: `${color}`, color: `${fontColor}`}}>
       <div className="title">
         <h3 style={{marginTop: '7%'}}>{note?.title}</h3>
       </div>
@@ -43,7 +55,10 @@ const RichText = () => {
       value={content}
       onChange={handleContent}
       />
-      <button  className="save" onClick={onClick}>save</button>
+      <div className="buttons">
+        <button  className="save" onClick={onTheme}>dark mode</button>
+        <button  className="save" onClick={onClick}>save</button>
+      </div>
     </div>
   )
 }
