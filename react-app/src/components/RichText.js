cdb77900b81c7bbfddf32dcef8845cd7190cca9c
Ali <ali.naqvi.dev@gmail.com>
2022-05-21 11:20:49 -0500

    fixed modal styling

--- react-app/src/components/RichText.js
+++ react-app/src/components/RichText.js
@@ -12,7 +12,7 @@ import Popup from "reactjs-popup";
 
 
 const RichText = () => {
-  
+
   const dispatch = useDispatch()
   const history = useHistory()
   const params = useParams()
@@ -23,8 +23,8 @@ const RichText = () => {
   const [color, setColor] = useState('#ebebeb')
   const [fontColor, setFontColor] = useState('darkslate')
 
-  const onClick = async () => { 
-    
+  const onClick = async () => {
+
     const updatedNote = {
       title: note?.title,
       content,
@@ -44,7 +44,7 @@ const RichText = () => {
     }
   }
 
-  const toggle = () => {setOpen(!open)}
+  const toggle = () => { setOpen(!open) }
 
   const onDelete = () => {
     dispatch(deleteNote(params.id))
@@ -64,25 +64,32 @@ const RichText = () => {
   }
 
   return (
-    <div id='container' style={{backgroundColor: `${color}`, color: `${fontColor}`}}>
+    <div id='container' style={{ backgroundColor: `${color}`, color: `${fontColor}` }}>
       <div className="title">
-        <h3 style={{marginTop: '7%'}}>{note?.title}</h3>
+        <h3 style={{ marginTop: '7%' }}>{note?.title}</h3>
       </div>
       <ReactQuill
-      style={{width: '65%', height: '100%'}}
-      placeholder="Write a new note." 
-      value={content}
-      onChange={handleContent}
+        style={{ width: '65%', height: '100%' }}
+        placeholder="Write a new note."
+        value={content}
+        onChange={handleContent}
       />
       <div className="buttons">
-        <button  className="save" onClick={onTheme}>toggle theme</button>
-        <button  className="save" onClick={onClick}>save</button>
+        <button className="save" onClick={onTheme}>toggle theme</button>
+        <button className="save" onClick={onClick}>save</button>
         <button className='save red' onClick={toggle}>delete</button>
         <Popup
-        open={open}
+          open={open}
+          onClose={() => setOpen(false)}
         >
-        <p>Are you sure?</p>
-        <button className="logout yes" onClick={onDelete}>Yes</button>
+
+          <div className="button-container" style={{ display: 'flex', flexDirection: "column", alignItems: "center", height: '200px', width: '300px' }}>
+            <p>Are you sure?</p>
+            <div className="button-container">
+              <button className="logout yes" style={{margin: '20px'}} onClick={onDelete}>Yes</button>
+              <button className="logout yes" style={{margin: '20px'}} onClick={() => setOpen(false)}>No</button>
+            </div>
+          </div>
         </Popup>
       </div>
     </div>
